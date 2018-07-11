d48efd4ea5dd0db3f87dc168f7b4426a145b7e65
Izaak van Dongen <izaak.van.dongen@gmail.com>
2018-07-11 23:20:33 +0100

    Add some documentation

--- src/interface.py
+++ src/interface.py
@@ -6,20 +6,26 @@ from base import Win
 from formatting import print_board, syms
 from checking import is_run
 
+# boolean-indexed array to get a string name for player
 name_from_bool = ["noughts", "crosses"]
 
+# dictionary to translate symbols to internal representation of tile states
 state_from_string = {"_": None, 'x': True, 'o': False}
 
 def isqrt(n):
+    """
+    Calculate the integer square root of a number using the "bit-shift"
+    algorithm.
+    """
     if n < 2:
         return n
     else:
-        smallCandidate = isqrt(n >> 2) << 1
-        largeCandidate = smallCandidate + 1
-        if largeCandidate ** 2 > n:
-            return smallCandidate
+        small = isqrt(n >> 2) << 1
+        large = small + 1
+        if large ** 2 > n:
+            return small
         else:
-            return largeCandidate
+            return large
 
 def SquareInt(s):
     """
@@ -31,29 +37,33 @@ def SquareInt(s):
     return n
 
 def SquareBoard(board):
+    """
+    Acts as a "parser" for strings representing square boards, similar to
+    SquareInt. Ignores all non-interesting characters and demands squareness.
+    """
     b = [state_from_string[c] for c in board if c in state_from_string]
     if isqrt(len(b)) ** 2 != len(b):
         raise ValueError('The board must be square')
     return b
 
-def get_pos(s):
+def get_pos(s, n):
     """
     Get position in 1d list from 2d coordinate reference
     """
     x, y = map(int, s.split())
-    if not all(0 <= c <= 2 for c in (x, y)):
-        raise ValueError("Not in range 0-2")
+    if not all(0 <= c < n for c in (x, y)):
+        raise ValueError("Not in range [0,{})".format(n))
 
-    return y * 3 + x
+    return y * n + x
 
-def get_input(board, is_crosses):
+def get_input(board, is_crosses, n):
     """
-    Get user input
+    Get user input of where to play on a board.
     """
     print("You are playing as {}".format(name_from_bool[is_crosses]))
     while True:
         try:
-            mov = get_pos(input("Enter the position you want to play in > "))
+            mov = get_pos(input("Enter the position you want to play in > "), n)
             if board[mov] is not None:
                 raise ValueError("This position is already taken")
         except ValueError as ve:
@@ -61,15 +71,16 @@ def get_input(board, is_crosses):
             continue
         return mov
 
-def do_player_move(board, is_crosses):
+def do_player_move(board, is_crosses, n):
     """
-    Execute player move - assumes board is valid at start of turn
+    Execute player move - assumes board is valid at start of turn.
     """
-    print_board(board)
+    print_board(board, n)
     try:
-        pos = get_input(board, is_crosses)
+        pos = get_input(board, is_crosses, n)
     except (KeyboardInterrupt, EOFError):
-        raise Win("\n{} wins because {} is a coward".format(syms[not is_crosses], syms[is_crosses]))
+        raise Win("\n{} wins because {} is a coward"
+                .format(syms[not is_crosses], syms[is_crosses]))
     board[pos] = is_crosses
-    if is_run(board, pos):
+    if is_run(board, pos, n):
         raise Win("{} wins".format(syms[is_crosses]))
--- src/play.py
+++ src/play.py
@@ -1,32 +1,43 @@
 """
-Play noughts and crosses
+Play noughts and crosses. Incorporates both human and computer players.
 """
 
 from argparse import ArgumentParser
 from itertools import cycle, repeat
 
 from base import GameFinish, Draw
-from interface import SquareInt, do_player_move
-from computer import do_computer_move
-from formatting import print_board
+from interface import do_player_move
+from computer import do_computer_move as _do_computer_move
 
 def get_args():
+    """
+    Get configuration for the program. See the help text for details.
+    """
     parser = ArgumentParser(description=__doc__)
-    parser.add_argument("-c", "--computer", action="store_true",
+    mode = parser.add_mutually_exclusive_group()
+    mode.add_argument("-c", "--computer", action="store_true",
                     help="play against computer opponent")
+    mode.add_argument("-b", "--battle", action="store_true",
+                    help="computer plays against itself")
     parser.add_argument("--headstart", action="store_true",
                     help="start first when playing against computer")
     parser.add_argument("--noughts-start", action="store_true",
                     help="noughts to start instead of crosses")
     parser.add_argument("-s", "--size", type=int, default=3,
                     help="size of board to play on")
+    parser.add_argument("-v", "--verbose", action="store_true",
+                    help="Show minmax thought process")
     return parser.parse_args()
 
-def play(board, players, noughts_start):
+def play(board, players, noughts_start, n):
+    """
+    Play a game of noughts and crosses until a finishing condition or a draw,
+    given an infinite iterable of players.
+    """
     is_crosses = not noughts_start
     try:
         for player in players:
-            player(board, is_crosses)
+            player(board, is_crosses, n)
             is_crosses = not is_crosses
             if board.count(None) == 0:
                 raise Draw("Nobody wins!")
@@ -35,11 +46,17 @@ def play(board, players, noughts_start):
 
 if __name__ == "__main__":
     args = get_args()
+    # Correctly initialise the infinite iterable of players according to
+    # arguments
+    vb = args.verbose
+    do_computer_move = lambda *args: _do_computer_move(*args, verbose=vb)
     if args.computer:
         if args.headstart:
             players = cycle([do_player_move, do_computer_move])
         else:
             players = cycle([do_computer_move, do_player_move])
+    elif args.battle:
+        players = repeat(do_computer_move)
     else:
         players = repeat(do_player_move)
-    play([None] * args.size ** 2, players, args.noughts_start)
+    play([None] * args.size ** 2, players, args.noughts_start, args.size)
